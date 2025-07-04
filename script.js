85705f35e1bcb01bfa97c771c8bd33e595520ef0
Aman-Bam <devimanju48830@gmail.com>
2025-07-04 23:15:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-/*Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
-
-toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
-notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
-
+/*
+Given an integer n, return a counter function. 
+This counter function initially returns n and then returns 1 more than 
+the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 */
-
-var expect = function(val) {
-    return {
-        toBe: function(expected) {
-            if (val === expected) return true;
-            throw new Error("Not Equal");
-        },
-        notToBe: function(expected) {
-            if (val !== expected) return true;
-            throw new Error("Equal");
-        }
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+        
     };
 };
-
